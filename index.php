c423f7306de9950729d945fb7234129e41008ddd
JoelB-E276 <joel.beauroy@outlook.fr>
2021-05-11 22:00:58 +0200

    add footer

---
+++ index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+
+    <title>My Bank</title>
+</head>
+
+<body>
+
+    <!-- <div id="layer">
+        <div id="layertxt"></div>
+        <div id="btnlayer" class="loadContent">Veuillez accepter pour accéder <button type="button" value="Accepter"
+                id="butn" class="btn btn-dark">Accepter</button>
+        </div>
+    </div>  /////////////////// IMPORTER FICHIER TXT POUR LAYER-->
+<?php include("nav.php");?>
+<?php include("header.php");?>
+<?php include("acounts.php");?>
+
+   
+
+    <main class="container my-5">
+        <h1>Vos comptes</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Compte courant</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Compte n° XXXXXXXXXXXX</h6>
+                            <p class="card-text"><strong>Solde</strong>&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; +
+                                12,95€</p>
+                            <a href="#" class="card-link ">Vos opérations</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Livret A</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Compte n° XXXXXXXXXXXX</h6>
+                            <p class="card-text"><strong>Solde</strong>&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; +
+                                246,50€ </p>
+                            <a href="#" class="card-link">Vos opérations</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">PEL</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Compte n° XXXXXXXXXXXX</h6>
+                            <p class="card-text"><strong>Solde</strong>&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; +
+                                1.750,00€ </p>
+                            <a href="#" class="card-link">Vos opérations</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Crédit Immobiler</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Compte n° XXXXXXXXXXXX</h6>
+                            <p class="card-text"><strong>Solde</strong>&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; -
+                                252.157,50€ </p>
+                            <a href="#" class="card-link">Vos opérations</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php include("footer.php");?>
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
+        crossorigin="anonymous"></script>
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    
+</body>
+
+</html>
