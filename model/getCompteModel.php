5b67e54ad3bf6fa09ce83efe207309db6701761b
Joel BE276 <joel.beauroy@outlook.fr>
2021-05-27 17:02:31 +0200

    PDO

--- model/getCompteModel.php
+++ model/getCompteModel.php
@@ -1,7 +1,7 @@
 <?php 
 
-function getCompte(PDO $db):array { 
-        $response =  $db->query("SELECT * FROM compte WHERE   "); 
+function getCompte(PDO $db):array {      // une prepare  +  FROM compte id=: variable
+        $response =  $db->query("SELECT * FROM compte");  
         $result = $response->fetchAll(PDO::FETCH_ASSOC); 
   
         return $result;                               
